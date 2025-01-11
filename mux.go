51c11a2750b06ad340a371a91a02871d714d2622
ishisora <tatsu.gami2573@gmail.com>
2025-01-11 17:10:32 +0900

    add health check endpoint

--- mux.go
+++ mux.go
@@ -9,6 +9,12 @@ import (
 
 func NewMux(ctx context.Context, cfg *config.Config) (*http.ServeMux, func(), error) {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("HealthCheck: OK"))
+	})
+
 	cleanup := func() {
 	}
 	return mux, cleanup, nil
